5395f8cfde2818a5bda21d1ec38f6456d9080412
ArthurMarianoNeto <thiesto@gmail.com>
2021-01-26 08:38:13 -0300

    Disponivel para testes com usuários

--- lib/user interface/home_page.dart
+++ lib/user interface/home_page.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
 //-------------------------------------------------  _getGifs   -------------------------------------------------------------
   Future<Map> _getGifs() async {
     http.Response response;
-    if (_search == null)
+    if(_search == null || _search.isEmpty)
       response = await http.get(
           "https://api.giphy.com/v1/gifs/trending?api_key=hERcFNNwE959tg9GF3qR34FFcFUypPgr&limit=20&rating=g");
     else
